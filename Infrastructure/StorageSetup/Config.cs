2ab161c4fadd1e4175fdae248f9c8e3cd2ea38f6
Paul Glavich <glav@theglavs.com>
2018-10-22 16:55:20 +1100

    Script re-org and addition of storage setup app

---
+++ Infrastructure/StorageSetup/Config.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageSetup
+{
+    public static class Config
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static Config()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            if (Environment.GetEnvironmentVariable("BuildConfiguration") == "development")
+            {
+                builder.AddUserSecrets<StorageSetup.Program>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public static string ConnectionStringStorageAccount => Configuration["ConnectionStrings:StorageAccount"];
+    }
+}
---
+++ Infrastructure/StorageSetup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StorageSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connection string: {0}", Config.ConnectionStringStorageAccount);
+        }
+    }
+}
