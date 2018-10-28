2a2a669fbf56a9ec0b182815481cd753fb57fea6
Paul Glavich <glav@theglavs.com>
2018-10-29 06:37:31 +1100

    Changed tio async and added some config

---
+++ Functions/MailCollectorFunction/Config/FunctionConfig.cs
@@ -0,0 +1,9 @@
+namespace MailCollectorFunction.Config
+{
+    public static class FunctionConfig
+    {
+        public const string QueueNameCollectEmail = "collectemail";
+        public const string QueueNameProcessEmail = "processemail";
+    }
+
+}
--- Functions/MailCollectorFunction/MailCollector.cs
+++ Functions/MailCollectorFunction/MailCollector.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using Core;
+using MailCollectorFunction.Config;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,12 +11,13 @@ namespace MailCollectorFunction
     public static class MailCollector
     {
         [FunctionName("MailCollector")]
-        public static void Run([QueueTrigger("myqueue-items", Connection = "")]string myQueueItem, ILogger log)
+        public static async Task Run([QueueTrigger(FunctionConfig.QueueNameCollectEmail)]string myQueueItem, ILogger log)
         {
             log.LogInformation("Starting MailCollector function");
             var dependencies = CoreDependencies.Setup(log);
 
-            dependencies.DiagnosticLogging.Info($"MailCollector Queue trigger function executed at: {DateTime.Now}");
+            dependencies.DiagnosticLogging.Info($"MailCollector Queue trigger function executed at: {DateTime.UtcNow}");
+
         }
     }
 }
