80590a3207708bb0b5b82a26987313020d49619f
Paul Glavich <glav@theglavs.com>
2018-11-08 18:17:59 +1100

    Adding proper async calls to mail client collection

--- Functions/MailCollectorFunction/Data/MailCollectionRepository.cs
+++ Functions/MailCollectorFunction/Data/MailCollectionRepository.cs
@@ -48,56 +48,54 @@ namespace MailCollectorFunction.Data
             }
         }
 
-        public Task<List<RawMailMessageEntity>> CollectMailAsync(EmailConfiguration emailConfig, int maxCount = EmailConfiguration.MaxEmailToRetrievePerCall)
+        public async Task<List<RawMailMessageEntity>> CollectMailAsync(EmailConfiguration emailConfig, int maxCount = EmailConfiguration.MaxEmailToRetrievePerCall)
         {
-            return Task.Run<List<RawMailMessageEntity>>(() =>
+            var emails = new List<RawMailMessageEntity>();
+            Dependencies.DiagnosticLogging.Info("Attempting to collect a maximum of {maxCount} emails", maxCount);
+
+            try
             {
-                var emails = new List<RawMailMessageEntity>();
-                Dependencies.DiagnosticLogging.Info("Attempting to collect a maximum of {maxCount} emails", maxCount);
-
-                try
+                using (var emailClient = new Pop3Client())
                 {
-                    using (var emailClient = new Pop3Client())
+                    var emailServer = $"[{emailConfig.PopServerHost}:{emailConfig.PopServerPort}]";
+                    Dependencies.DiagnosticLogging.Verbose("Collecting mail from Host:{emailServer}", emailServer);
+
+                    emailClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                    await emailClient.ConnectAsync(emailConfig.PopServerHost, emailConfig.PopServerPort, SecureSocketOptions.Auto);
+
+                    emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                    Dependencies.DiagnosticLogging.Info($"Authenticating to email server {emailServer}, : Username: [{emailConfig.Username}]");
+
+                    await emailClient.AuthenticateAsync(emailConfig.Username, emailConfig.Password);
+                    var msgCount = emailClient.Count;
+
+                    Dependencies.DiagnosticLogging.Info($"Successfully authenticated to email server:{emailServer}, {msgCount} mail msgs in queue");
+
+                    for (int i = 0; i < msgCount && i < maxCount; i++)
                     {
-                        var emailServer = $"[{emailConfig.PopServerHost}:{emailConfig.PopServerPort}]";
-                        Dependencies.DiagnosticLogging.Verbose("Collecting mail from Host:{emailServer}", emailServer);
-
-                        emailClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
-                        emailClient.Connect(emailConfig.PopServerHost, emailConfig.PopServerPort, SecureSocketOptions.Auto);
-
-                        emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
-
-                        Dependencies.DiagnosticLogging.Info($"Authenticating to email server {emailServer}, : Username: [{emailConfig.Username}]");
-
-                        emailClient.Authenticate(emailConfig.Username, emailConfig.Password);
-                        var msgCount = emailClient.Count;
-
-                        Dependencies.DiagnosticLogging.Info($"Successfully authenticated to email server:{emailServer}, {msgCount} mail msgs in queue");
-
-                        for (int i = 0; i < msgCount && i < maxCount; i++)
+                        var message = await emailClient.GetMessageAsync(i);
+                        var emailMessage = new RawMailMessageEntity
                         {
-                            var message = emailClient.GetMessage(i);
-                            var emailMessage = new RawMailMessageEntity
-                            {
-                                Body = !string.IsNullOrEmpty(message.HtmlBody) ? message.HtmlBody : message.TextBody,
-                                Subject = message.Subject
-                            };
-                            emailMessage.ToAddresses.AddRange(message.To.Select(x => (MailboxAddress)x).Select(x => new RawEmailAddress { Address = x.Address, Name = x.Name }));
-                            emailMessage.FromAddresses.AddRange(message.From.Select(x => (MailboxAddress)x).Select(x => new RawEmailAddress { Address = x.Address, Name = x.Name }));
-                            emails.Add(emailMessage);
-                        }
-
-                        Dependencies.DiagnosticLogging.Info($"Collected {emails.Count} emails from server.");
-
-                        return emails;
+                            Body = !string.IsNullOrEmpty(message.HtmlBody) ? message.HtmlBody : message.TextBody,
+                            Subject = message.Subject
+                        };
+                        emailMessage.ToAddresses.AddRange(message.To.Select(x => (MailboxAddress)x).Select(x => new RawEmailAddress { Address = x.Address, Name = x.Name }));
+                        emailMessage.FromAddresses.AddRange(message.From.Select(x => (MailboxAddress)x).Select(x => new RawEmailAddress { Address = x.Address, Name = x.Name }));
+                        emails.Add(emailMessage);
                     }
-                }
-                catch (Exception ex)
-                {
-                    Dependencies.DiagnosticLogging.Fatal(ex, "Error attempting to collect mail");
+
+                    await emailClient.DisconnectAsync(true);
+                    Dependencies.DiagnosticLogging.Info($"Collected {emails.Count} emails from server.");
+
                     return emails;
                 }
-            });
+            }
+            catch (Exception ex)
+            {
+                Dependencies.DiagnosticLogging.Fatal(ex, "Error attempting to collect mail");
+                return emails;
+            }
         }
 
         public async Task LodgeMailCollectedAcknowledgementAsync(GenericActionMessage receivedMessage)
